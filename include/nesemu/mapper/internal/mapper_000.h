137e5a2c438c1951b14dd4e37a8c00796efdef02
Matt Reynolds <mtreynolds@uwaterloo.ca>
2020-05-06 02:17:02 -0400

    Use 32bit address for mapped addresses

--- include/nesemu/mapper/internal/mapper_000.h
+++ include/nesemu/mapper/internal/mapper_000.h
@@ -9,7 +9,7 @@ class Mapper000 : public Mapper {
 public:
   using Mapper::Mapper;  // "Inherit" constructor
 
-  uint16_t decodeCPUAddress(uint16_t addr) const override { return addr & (prg_banks_ > 1 ? 0x7FFF : 0x3FFF); };
+  uint32_t decodeCPUAddress(uint16_t addr) const override { return addr & (prg_banks_ > 1 ? 0x7FFF : 0x3FFF); };
 };
 
 }  // namespace mapper::internal
--- include/nesemu/mapper/mapper_base.h
+++ include/nesemu/mapper/mapper_base.h
@@ -13,13 +13,13 @@ public:
   explicit Mapper(uint8_t prg_banks, uint8_t chr_banks) : prg_banks_(prg_banks), chr_banks_(chr_banks) {};
   virtual ~Mapper() = default;
 
-  virtual uint16_t decodeCPUAddress(uint16_t addr) const { return addr - PRG_ROM_OFFSET; };
-  virtual uint16_t decodePPUAddress(uint16_t addr) const { return addr - CHR_ROM_OFFSET; };
+  virtual uint32_t decodeCPUAddress(uint16_t addr) const { return addr - PRG_ROM_OFFSET; };
+  virtual uint32_t decodePPUAddress(uint16_t addr) const { return addr - CHR_ROM_OFFSET; };
   virtual void     write(uint16_t /*addr*/, uint8_t /*data*/) {};
 
 protected:
   uint8_t prg_banks_;  ///< Number of 8KiB PRG ROM banks
-  uint8_t chr_banks_;  ///< Number of 8KiB CHR ROM banks
+  uint8_t chr_banks_;  ///< Number of 8KiB CHR ROM/RAM banks
 };
 
 }  // namespace mapper
