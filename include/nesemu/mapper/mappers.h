9d2892b32d243ba09527ac372b0a15bcce9fc212
Matt Reynolds <mtreynolds@uwaterloo.ca>
2020-05-14 22:33:58 -0400

    Add mappers 002 and 003

--- include/nesemu/mapper/mappers.h
+++ include/nesemu/mapper/mappers.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "internal/mapper_000.h"
+#include "internal/mapper_002.h"
 #include "internal/mapper_001.h"
+#include "internal/mapper_003.h"
 #include "internal/mapper_004.h"
 #include <nesemu/logger.h>
 #include <nesemu/mapper/mapper_base.h>
@@ -28,8 +30,8 @@ Mapper* dummy(uint8_t /*prg_banks*/, uint8_t /*chr_banks*/) {
 Mapper* (*mappers[])(uint8_t, uint8_t) = {
     internal::make<internal::Mapper000>,  // Mapper 000 - Nintendo NROM
     internal::make<internal::Mapper001>,  // Mapper 001 - Nintendo MMC1
-    internal::dummy<2>,                   // Mapper 002
-    internal::dummy<3>,                   // Mapper 003
+    internal::make<internal::Mapper002>,  // Mapper 002 - Nintendo UxROM
+    internal::make<internal::Mapper003>,  // Mapper 003 - Nintendo CNROM
     internal::make<internal::Mapper004>,  // Mapper 004 - Nintendo MMC3
     internal::dummy<5>,                   // Mapper 005
     internal::dummy<6>,                   // Mapper 006
