dc8be5980035bdb7212cf2101e70cc3f17cde45d
Matt Reynolds <mtreynolds@uwaterloo.ca>
2022-07-10 21:56:03 -0700

    Fix buffer overrun and add sample dropper

--- src/ui/speaker.cpp
+++ src/ui/speaker.cpp
@@ -7,15 +7,31 @@
 // TODO: Make circular buffer class
 constexpr size_t AUDIO_BUFFER_LEN = (1 << 13);  // (2^13)/48000 ~= 0.175sec
 uint8_t          audio_buffer[AUDIO_BUFFER_LEN];
-size_t           audio_buffer_head = {0};
-size_t           audio_buffer_tail = {0};
+size_t           audio_buffer_head = {0};  // read
+size_t           audio_buffer_tail = {0};  // write
 size_t           audio_buffer_size = {0};
+
+// Statistics
+size_t dropped_samples  = {0};
+size_t inserted_samples = {0};
 
 extern "C" void audio_callback(void* userdata, uint8_t* stream, int len) {
   (void) userdata;
   if (audio_buffer_size == 0) {
-    // No salvaging this :/
+    // No salvaging this, nothing to stretch :/
+    inserted_samples += len;
     return;
+  }
+
+  static int count = 0;
+  count            = (count + 1) % 10;
+  if (count == 0) {
+    logger::log<logger::DEBUG_APU>("Buffer size: %u/%u=%f\t\tDropped:%u\tInserted:%u\n",
+                                   audio_buffer_size,
+                                   AUDIO_BUFFER_LEN,
+                                   (float) (audio_buffer_size) / AUDIO_BUFFER_LEN,
+                                   dropped_samples,
+                                   inserted_samples);
   }
 
   size_t copied     = std::min<size_t>(len, audio_buffer_size);
@@ -25,7 +41,24 @@ extern "C" void audio_callback(void* userdata, uint8_t* stream, int len) {
   audio_buffer_head = (audio_buffer_head + copied) % AUDIO_BUFFER_LEN;
   audio_buffer_size -= copied;
 
-  // Hacky audio smoothing. Replay the last chunk of samples back and forth to fill up the audio buffer. This sounds
+  // Drop every DROP_PERIODth sample from the buffer to keep some headroom in the buffer.
+  // This compensates for extra frames added when the buffer runs dry or the system runs over 100% speed (uncapped)
+  // Headroom in the buffer helps to smooth audio out in these cases, rather than just jumping ahead several samples if
+  // the writer overwrites the read head and pushes the reader forward
+  constexpr size_t DROP_PERIOD = 150;
+  const size_t     target      = std::max<size_t>(AUDIO_BUFFER_LEN * 0.85, len);
+  if (audio_buffer_size + copied > target) {
+    size_t available = audio_buffer_size + copied - target;
+    for (size_t i = 1; i <= available && i * DROP_PERIOD < len; i++) {
+      SDL_memmove(stream + (i * DROP_PERIOD), stream + (i * DROP_PERIOD) + 1, len - (i * DROP_PERIOD + 1));
+      stream[len - 1]   = audio_buffer[audio_buffer_head];
+      audio_buffer_head = (audio_buffer_head + 1) % AUDIO_BUFFER_LEN;
+      audio_buffer_size--;
+      dropped_samples++;
+    }
+  }
+
+  // Hacky audio stretching. Replay the last chunk of samples back and forth to fill up the audio buffer. This sounds
   // pretty bad, but it's better than the crackles and pops you get if filled with silence.
   while (copied < len) {
     int to_copy = std::min(copied, (len - copied));
@@ -33,6 +66,7 @@ extern "C" void audio_callback(void* userdata, uint8_t* stream, int len) {
       stream[copied + i] = stream[copied - 1 - i];
     }
     copied += to_copy;
+    inserted_samples += to_copy;
   }
 }
 
@@ -101,15 +135,19 @@ void ui::Speaker::update(uint8_t* stream, size_t len) {
          && SDL_AudioStreamGet(downsampler_b_, buffer, available)) {
     SDL_LockAudioDevice(device_);
     // Overwrite oldest samples if needed
-    if (audio_buffer_size == AUDIO_BUFFER_LEN) {
-      audio_buffer_head = (audio_buffer_head + available) % AUDIO_BUFFER_LEN;
+    if (audio_buffer_size + available > AUDIO_BUFFER_LEN) {
+      audio_buffer_head = (audio_buffer_tail + available) % AUDIO_BUFFER_LEN - 1;
     }
 
     size_t copy_a_len = std::min(available, AUDIO_BUFFER_LEN - audio_buffer_tail);
     memcpy(audio_buffer + audio_buffer_tail, buffer, copy_a_len);
     memcpy(audio_buffer, buffer + copy_a_len, available - copy_a_len);
     audio_buffer_tail = (audio_buffer_tail + available) % AUDIO_BUFFER_LEN;
-    audio_buffer_size += available;
+    audio_buffer_size = std::min(audio_buffer_size + available, AUDIO_BUFFER_LEN);
     SDL_UnlockAudioDevice(device_);
   }
+
+  // TODO: 90Hz first-order high-pass
+  // TODO: 440Hz first-order high-pass
+  // TODO: 14kHz first-order low-pass
 }
