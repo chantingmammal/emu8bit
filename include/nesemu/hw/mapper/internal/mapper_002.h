2588223f927777988d7aefacd2e0bd10e42f1c77
Matt Reynolds <mtreynolds@uwaterloo.ca>
2022-07-12 23:48:40 -0700

    Fix mapper 2

--- include/nesemu/hw/mapper/internal/mapper_002.h
+++ include/nesemu/hw/mapper/internal/mapper_002.h
@@ -12,13 +12,13 @@ public:
   uint32_t decodeCPUAddress(uint16_t addr) const override {
     uint8_t bank_num = 0;
 
-    if (addr < 0xC000) {                // 0x8000-0xBFFF
-      bank_num = prg_bank_;             //
-    } else {                            // 0xC000-0xFFFF
-      bank_num = (prg_banks_ * 2) - 1;  //
+    if (addr < 0xC000) {          // 0x8000-0xBFFF
+      bank_num = prg_bank_;       //
+    } else {                      // 0xC000-0xFFFF
+      bank_num = prg_banks_ - 1;  //
     }
 
-    return (0x2000 * bank_num) | (addr & 0x1FFF);
+    return (0x4000 * bank_num) | (addr & 0x3FFF);
   };
 
   void write(uint16_t /*addr*/, uint8_t data) override { prg_bank_ = data; };
