c010f9971ae65aedfd149894ab2ff277ec6ac7c8
Matt Reynolds <mtreynolds@uwaterloo.ca>
2022-07-16 01:02:05 -0700

    Overhaul audio to improve performance

--- include/nesemu/ui/speaker.h
+++ include/nesemu/ui/speaker.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <algorithm>  // std::min
+#include <cstddef>    // size_t
 #include <cstdint>    // uint8_t, uint16_t
 
 #include <SDL2/SDL.h>
 
 
 namespace ui {
+
+class Speaker;
+void audio_callback(Speaker* speaker, uint8_t* stream, size_t len);
 
 class Speaker {
 public:
@@ -18,6 +22,8 @@ public:
   void pause(bool pause);
 
 private:
+  friend void audio_callback(Speaker* speaker, uint8_t* stream, size_t len);
+
   // We need to downsample from ~1.79MHz to a standard freq like 44.1kHz. Since the downsampler only supports integral
   // sampling rates, we first upsample by 11 to achieve an integral rate, then downsample to the output freq
   static constexpr int             UPSAMPLE        = 11;
@@ -26,7 +32,22 @@ private:
   static constexpr int             OUTPUT_C        = 44100;      // Output freq
   static constexpr SDL_AudioFormat OUTPUT_FORMAT   = AUDIO_U8;   // Unsigned 8-bit samples
   static constexpr uint8_t         OUTPUT_CHANNELS = 1;          // Mono
-  static constexpr uint16_t        OUTPUT_SAMPLES  = (1 << 12);  // Buffer size, in samples
+  static constexpr uint16_t        OUTPUT_SAMPLES  = (1 << 11);  // Buffer size, in samples
+                                                                 //   Seem to always get 2x less than requested
+
+  // We ensure the target audio buffer len is at least as long as the output device sample buffer, so that the audio
+  // device never runs dry.
+  //
+  // The resulting latency is the processing time of the downsampler plus the internal buffer size of the
+  // downsampler. This is approx 0.07sec, or 1/15th of a sec.
+  //
+  //  - The downsampler processing time has not yet been measured
+  //  - The upsampler outputs every 100 samples = 100*(22/39375000) ~= 0.000056sec
+  //  - The downsampler starts outputting after 41560 input samples = 41560*(22/39375000) ~= 0.023sec
+  //  - The TARGET_AUDIO_BUFFER_LEN = (2^11)/44100 ~= 0.045sec
+  //  - The OUTPUT_SAMPLES = (2^10)/44100 ~= 0.023sec, irrelevant since TARGET_AUDIO_BUFFER_LEN >= OUTPUT_SAMPLES
+  static constexpr size_t TARGET_AUDIO_BUFFER_LEN = (1 << 11);
+  static_assert(TARGET_AUDIO_BUFFER_LEN >= OUTPUT_SAMPLES);
 
   float volume_ = 0.5;
 
--- src/ui/speaker.cpp
+++ src/ui/speaker.cpp
@@ -4,23 +4,17 @@
 #include <cstddef>
 #include <cstdint>
 
-// TODO: Make circular buffer class
-constexpr size_t AUDIO_BUFFER_LEN = (1 << 13);  // (2^13)/48000 ~= 0.175sec
-uint8_t          audio_buffer[AUDIO_BUFFER_LEN];
-size_t           audio_buffer_head = {0};  // read
-size_t           audio_buffer_tail = {0};  // write
-size_t           audio_buffer_size = {0};
+extern "C" void cb_trampoline(void* userdata, uint8_t* stream, int len) {
+  ui::Speaker* speaker = static_cast<ui::Speaker*>(userdata);
+  ui::audio_callback(speaker, stream, len);
+}
 
-// Statistics
-size_t dropped_samples  = {0};
-size_t inserted_samples = {0};
-
-extern "C" void audio_callback(void* userdata, uint8_t* stream, int len) {
-  (void) userdata;
+void ui::audio_callback(ui::Speaker* speaker, uint8_t* stream, size_t len) {
+  const size_t available = SDL_AudioStreamAvailable(speaker->downsampler_);
 
   static bool startup_complete = false;
   if (!startup_complete) {
-    if (audio_buffer_size < len * 1.5) {
+    if (available < ui::Speaker::TARGET_AUDIO_BUFFER_LEN) {
       memset(stream, 0, len);
       return;
     } else {
@@ -28,57 +22,38 @@ extern "C" void audio_callback(void* userdata, uint8_t* stream, int len) {
     }
   }
 
-  if (audio_buffer_size == 0) {
-    // No salvaging this, nothing to stretch :/
-    inserted_samples += len;
+
+  // No salvaging this, nothing to stretch :/
+  if (available == 0) {
     memset(stream, 0, len);
     return;
   }
 
-  static int count = 0;
-  count            = (count + 1) % 10;
-  if (count == 0) {
-    logger::log<logger::DEBUG_APU>("Buffer size: %u/%u=%f\t\tDropped:%u\tInserted:%u\n",
-                                   audio_buffer_size,
-                                   AUDIO_BUFFER_LEN,
-                                   (float) (audio_buffer_size) / AUDIO_BUFFER_LEN,
-                                   dropped_samples,
-                                   inserted_samples);
-  }
+  // Fill the device audio buffer
+  size_t copied = std::min<size_t>(len, available);
+  SDL_AudioStreamGet(speaker->downsampler_, stream, copied);
 
-  size_t copied     = std::min<size_t>(len, audio_buffer_size);
-  size_t copy_a_len = std::min<size_t>(copied, AUDIO_BUFFER_LEN - audio_buffer_head);
-  SDL_memcpy(stream, audio_buffer + audio_buffer_head, copy_a_len);
-  SDL_memcpy(stream + copy_a_len, audio_buffer, copied - copy_a_len);
-  audio_buffer_head = (audio_buffer_head + copied) % AUDIO_BUFFER_LEN;
-  audio_buffer_size -= copied;
-
-  // Drop every DROP_PERIODth sample from the buffer to keep some headroom in the buffer.
+  // Drop samples as required from the input buffer to maintain the target buffer length
   // This compensates for extra frames added when the buffer runs dry or the system runs over 100% speed (uncapped)
-  // Headroom in the buffer helps to smooth audio out in these cases, rather than just jumping ahead several samples if
-  // the writer overwrites the read head and pushes the reader forward
-  constexpr size_t DROP_PERIOD = 150;
-  const size_t     target      = std::max<size_t>(AUDIO_BUFFER_LEN * 0.85, len);
-  if (audio_buffer_size + copied > target) {
-    size_t available = audio_buffer_size + copied - target;
-    for (size_t i = 1; i <= available && i * DROP_PERIOD < len; i++) {
-      SDL_memmove(stream + (i * DROP_PERIOD), stream + (i * DROP_PERIOD) + 1, len - (i * DROP_PERIOD + 1));
-      stream[len - 1]   = audio_buffer[audio_buffer_head];
-      audio_buffer_head = (audio_buffer_head + 1) % AUDIO_BUFFER_LEN;
-      audio_buffer_size--;
-      dropped_samples++;
+  if (available > ui::Speaker::TARGET_AUDIO_BUFFER_LEN) {
+    const size_t to_drop     = available - ui::Speaker::TARGET_AUDIO_BUFFER_LEN;
+    const size_t drop_period = std::floor(static_cast<double>(to_drop) / len);
+
+    for (size_t i = 1; i <= to_drop && i * drop_period < len; i++) {
+      SDL_memmove(stream + (i * drop_period), stream + (i * drop_period) + 1, len - (i * drop_period + 1));
+      SDL_AudioStreamGet(speaker->downsampler_, &stream[len - 1], 1);
     }
   }
 
   // Hacky audio stretching. Replay the last chunk of samples back and forth to fill up the audio buffer. This sounds
   // pretty bad, but it's better than the crackles and pops you get if filled with silence.
+  const size_t repeat_buffer = copied;
   while (copied < len) {
-    int to_copy = std::min(copied, (len - copied));
+    int to_copy = std::min(repeat_buffer, (len - copied));
     for (int i = 0; i < to_copy; i++) {
       stream[copied + i] = stream[copied - 1 - i];
     }
     copied += to_copy;
-    inserted_samples += to_copy;
   }
 }
 
@@ -89,7 +64,8 @@ bool ui::Speaker::init() {
   desired_spec.format   = OUTPUT_FORMAT;
   desired_spec.channels = OUTPUT_CHANNELS;
   desired_spec.samples  = OUTPUT_SAMPLES;
-  desired_spec.callback = audio_callback;
+  desired_spec.callback = cb_trampoline;
+  desired_spec.userdata = this;
 
   // Attach audio device
   if (!(device_ = SDL_OpenAudioDevice(NULL, 0, &desired_spec, &audio_spec_, SDL_AUDIO_ALLOW_ANY_CHANGE))) {
@@ -120,36 +96,25 @@ void ui::Speaker::close() {
 }
 
 void ui::Speaker::update(uint8_t* stream, size_t len) {
+  constexpr size_t UPSAMPLE_B = 100;
+  static uint8_t   upsample_buffer[UPSAMPLE * UPSAMPLE_B];
+  static uint8_t   buffer_size = 0;
+
   // Upscale by 11 and feed the downsampler
-  // Note that feeding the downsampler is slow, so feed it 11 at a time
-  // We could consider further slowing this down
+  // Note that feeding the downsampler is slow, so feed it 11*10 at a time
   for (size_t i = 0; i < len; i++) {
-    static uint8_t upsample_buffer[UPSAMPLE];
-    SDL_memset(upsample_buffer, stream[i] * volume_, UPSAMPLE);
-    if (0 != SDL_AudioStreamPut(downsampler_, upsample_buffer, UPSAMPLE)) {
-      logger::log<logger::ERROR>("Failed to put samples in first downsampler: %s\n", SDL_GetError());
+    SDL_memset(&upsample_buffer[UPSAMPLE * buffer_size++], stream[i] * volume_, UPSAMPLE);
+
+    if (buffer_size == UPSAMPLE_B) {
+      SDL_LockAudioDevice(device_);
+      if (0 != SDL_AudioStreamPut(downsampler_, upsample_buffer, UPSAMPLE * UPSAMPLE_B)) {
+        logger::log<logger::ERROR>("Failed to put samples in first downsampler: %s\n", SDL_GetError());
+      }
+      SDL_UnlockAudioDevice(device_);
+      buffer_size = 0;
     }
   }
 
-  // When the downsampler has output, feed the output buffer
-  constexpr size_t BUFFER_LEN = 1024;
-  static uint8_t   buffer[BUFFER_LEN];
-  size_t           available = 0;
-  while ((available = std::min<size_t>(SDL_AudioStreamAvailable(downsampler_), BUFFER_LEN))
-         && SDL_AudioStreamGet(downsampler_, buffer, available)) {
-    SDL_LockAudioDevice(device_);
-    // Overwrite oldest samples if needed
-    if (audio_buffer_size + available > AUDIO_BUFFER_LEN) {
-      audio_buffer_head = (audio_buffer_tail + available) % AUDIO_BUFFER_LEN - 1;
-    }
-
-    size_t copy_a_len = std::min(available, AUDIO_BUFFER_LEN - audio_buffer_tail);
-    memcpy(audio_buffer + audio_buffer_tail, buffer, copy_a_len);
-    memcpy(audio_buffer, buffer + copy_a_len, available - copy_a_len);
-    audio_buffer_tail = (audio_buffer_tail + available) % AUDIO_BUFFER_LEN;
-    audio_buffer_size = std::min(audio_buffer_size + available, AUDIO_BUFFER_LEN);
-    SDL_UnlockAudioDevice(device_);
-  }
 
   // TODO: 90Hz first-order high-pass
   // TODO: 440Hz first-order high-pass
